35917a987867ad4a69dcfa27a63c8678f8bddba1
Aldebaraan <aldecanedo97@gmail.com>
2025-01-12 13:29:32 -0500

    .

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ class Program
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.White);
 
-            Raylib.DrawText("Alo alo!", 12, 12, 20, Color.Black);
+            Raylib.DrawText("testing", 12, 12, 20, Color.Black);
 
             Raylib.EndDrawing();
         }
