1831e252f75a48c9044e8edcb6fefb378c51bcd3
Aldebaraan <aldecanedo97@gmail.com>
2025-01-12 23:22:46 -0500

    Added textures

--- Program.cs
+++ Program.cs
@@ -6,21 +6,43 @@ namespace HelloWorld;
 
 class Program
 {
-
     public static void Main()
     {
+        int screenHeigth;
+        int screenWidth;
         Console.WriteLine("Hello World!");
-        Raylib.InitWindow(800, 480, "Hello World!");
+        Raylib.InitWindow(800, 800, "Hello World!");
+        Texture2D grass = Raylib.LoadTexture("floor_tiles/grass_01.png");
 
         while (!Raylib.WindowShouldClose())
         {
+            screenHeigth = Raylib.GetScreenHeight();
+            screenWidth = Raylib.GetScreenWidth();
+
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.White);
 
-            Raylib.DrawText("change01", 12, 12, 20, Color.Black);
+            makeGrid(screenHeigth, screenWidth);
+            for (int i = 0; i < screenWidth; i += 16) {
+                for (int j = 0; j < screenHeigth; j += 16) {
+                    Raylib.DrawTexture(grass, i, j, Color.White);
+                    }
+            }
+             Raylib.DrawText("Test_map", 12, 12, 20, Color.Black);
+
 
             Raylib.EndDrawing();
         }
         Raylib.CloseWindow();
+    }
+
+    // Draw a grid of lines
+    public static void makeGrid(int screenHeigth, int screenWidth) {
+        for (int i = 0; i < screenHeigth; i+=16) {
+            Raylib.DrawLine(0, i, screenWidth, i, Color.Black);
+        }
+        for (int i = 0; i < screenWidth; i+=16) {
+            Raylib.DrawLine(i, 0, i, screenHeigth, Color.Black);
+        }
     }
 }
